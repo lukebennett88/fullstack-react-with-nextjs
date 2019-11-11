e66e8814cd6536ef0bae13532bd271036fb89fe3
Luke Bennett <hello@lukebennett.com.au>
2019-11-11 22:38:46 +1100

    Add .env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 const withCSS = require('@zeit/next-css');
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 
-module.exports = withCSS({});
+module.exports = withCSS({
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    return config;
+  },
+});
