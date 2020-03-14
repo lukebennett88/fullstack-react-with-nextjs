42f087dec3965e57de81528c9d9b3f9cea40105c
Luke Bennett <hello@lukebennett.com.au>
2020-03-14 22:29:23 +1100

    Update react-hook-form API

--- src/components/habit-form.js
+++ src/components/habit-form.js
@@ -1,6 +1,6 @@
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import useForm from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { FiAlertTriangle } from 'react-icons/fi';
 
 const HabitForm = () => {
@@ -34,7 +34,7 @@ const HabitForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center">
       {errors.habit && (
-        <p className="absolute flex font-black items-center text-pink-400 px-4 top-0 -mt-8">
+        <p className="absolute top-0 flex items-center px-4 -mt-8 font-black text-pink-400">
           <FiAlertTriangle className="text-pink-300" />{' '}
           <span className="ml-1">Please add a new habit</span>
         </p>
@@ -44,11 +44,11 @@ const HabitForm = () => {
         placeholder="Habit"
         name="habit"
         ref={register({ required: true, maxLength: 80 })}
-        className="bg-blue-800 border-2 border-pink-400 font-black px-4 py-1 rounded text-pink-400 placeholder-pink-400 shadow hover:shadow-lg"
+        className="px-4 py-1 font-black text-pink-400 placeholder-pink-400 bg-blue-800 border-2 border-pink-400 rounded shadow hover:shadow-lg"
       />
       <button
         type="submit"
-        className="bg-pink-400 font-black ml-4 px-4 py-1 rounded shadow text-pink-800 hover:bg-pink-500 hover:shadow-lg hover:text-pink-900"
+        className="px-4 py-1 ml-4 font-black text-pink-800 bg-pink-400 rounded shadow hover:bg-pink-500 hover:shadow-lg hover:text-pink-900"
       >
         Submit
       </button>
