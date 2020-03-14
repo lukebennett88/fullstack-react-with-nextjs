28b22e8bf0f75562f4e24513c5b03241de0ee73f
Luke Bennett <hello@lukebennett.com.au>
2020-03-14 22:29:34 +1100

    Change margin to padding

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 const Nav = () => (
-  <nav className="px-6 py-4">
+  <nav className="mt-12 px-6 py-4">
     <h1 className="font-black text-4xl text-center text-pink-400">
       Level Up Your Life
     </h1>
